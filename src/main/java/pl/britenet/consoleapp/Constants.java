34e8ccea9f9a9cb107baba5a495c8ceaf9e751dd
Jakub Zelmanowicz <jakub.zelmanowicz@britenet.com.pl>
2022-06-07 12:06:31 +0200

    Zajęcia z 07.06.2022

---
+++ src/main/java/pl/britenet/consoleapp/Constants.java
@@ -0,0 +1,7 @@
+package pl.britenet.consoleapp;
+
+public class Constants {
+
+    public static final String COMMAND_NAME_HELP = "help";
+
+}
---
+++ src/main/java/pl/britenet/consoleapp/Main.java
@@ -0,0 +1,35 @@
+package pl.britenet.consoleapp;
+
+import pl.britenet.consoleapp.obj.command.Command;
+import pl.britenet.consoleapp.obj.command.HelpCommand;
+import pl.britenet.consoleapp.service.CommandService;
+
+import java.util.Optional;
+import java.util.Scanner;
+
+public class Main {
+
+    public static boolean IS_ON = true;
+
+    public static void main(String[] args) {
+        CommandService commandService = new CommandService();
+        commandService.registerCommand(new HelpCommand(commandService));
+
+        Scanner scanner = new Scanner(System.in);
+        while (IS_ON) {
+
+            System.out.println("Podaj komende:");
+            String commandName = scanner.nextLine();
+            Optional<Command> optionalCommand = commandService.findCommand(commandName);
+
+            if (optionalCommand.isPresent()) {
+                optionalCommand.get().execute();
+            }
+            else {
+                System.out.println("Nieznana komenda");
+            }
+
+        }
+    }
+
+}
